20c4fd046820ba592bb522c6f4c35fe6604f1e94
DavideCiammetti <davideciammetti97@gmail.com>
2024-10-30 22:03:40 +0100

    scaffolding

---
+++ tehlp-core/src/main/java/it/tehlp/TehlpBeApplication.java
@@ -0,0 +1,13 @@
+package it.tehlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TehlpBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TehlpBeApplication.class, args);
+	}
+
+}
